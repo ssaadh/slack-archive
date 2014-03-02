ef3e5abacdef507aacfb2fce657f2e3a78f690ee
iknowicouldalwaysturn <iknowicouldalwaysturn@gmail.com>
2014-03-02 00:42:53 -0500

    Working method to retrieve messages for a channel. Not dry and has annoyances like going through all messages when there is just a single new message. But working.

--- app.rb
+++ app.rb
@@ -40,12 +40,60 @@ def slack_api_lists( method_name, response_hash, model_class )
   return 'Done'
 end
 
-get '/slack/channel/:name' do
-  history = @slack.channel_history params[ :name ]
-  return 'API call failed' if history[ 'ok' ] == false
+get '/slack/channel/messages/:name' do
+  current_channel = Channel.find_by( name: params[ :name ] )
+  history = nil
+  last_message = 0
+  has_more = true
+  first_timestamp = nil  
   
-  current_history = history[ 'messages' ]
-  has_more = history[ 'has_more' ]
+  count = 0
+  loop do
+    last_message = history[ 'messages' ].last[ 'ts' ] if !history.nil?
+    
+    # api call
+    history = @slack.channel_history current_channel.slack_id, last_message
+    return 'API call failed' if history[ 'ok' ] == false
+    
+    # assign
+    current_history = history[ 'messages' ]
+    has_more = history[ 'has_more' ]
+    
+    first_timestamp = current_history.first[ 'ts' ].to_f if count == 0
+    
+    # see if latest message has already been retrieved
+    if !current_channel.last_check.nil?
+      break if current_channel.last_check >= first_timestamp
+    end
+    
+    # loop over messages
+    current_history.each do |individual|
+      next if individual[ 'hidden' ] == true
+            
+      subtype = nil
+      subtype = individual[ 'subtype' ] if individual.has_key? 'subtype'      
+      
+      if subtype == 'file_comment'
+        user = User.find_by( slack_id: individual[ 'comment' ][ 'user' ] )
+        text = individual[ 'comment' ][ 'comment' ]
+      else
+        user = User.find_by( slack_id: individual[ 'user' ] )
+        text = individual[ 'text' ]
+      end
+      
+      Message.create( channel_id: current_channel.id, user_id: user.id, subtype: subtype, text: text, timestamp: individual[ 'ts' ].to_f )
+    end
+    
+    count += 1
+    break if has_more == false
+  end
+  
+  # Thinking is, if code breaks at some point in between and not all older messages have been saved to the database, this value won't get updated.
+  # So next time, method will go through messages again instead of just stopping because the database has the most recent message.
+  current_channel.last_check = first_timestamp if !first_timestamp.nil?
+  current_channel.save
+  
+  return 'Done'
 end
 
 ##
