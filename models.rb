9eb1d5b7480f2a46116d3f3261f1642749d8d7c6
iknowicouldalwaysturn <iknowicouldalwaysturn@gmail.com>
2014-03-02 00:34:11 -0500

    Minor bugs/typos with way associations were being done

--- models.rb
+++ models.rb
@@ -1,19 +1,19 @@
 class User < ActiveRecord::Base
-  has_one :message
+  has_many :messages
   
   validates :slack_id, uniqueness: true
 end
 
 class Channel < ActiveRecord::Base
-  has_one :message
+  has_many :messages
   
   validates :slack_id, uniqueness: true
 end
 
 class Message < ActiveRecord::Base
-  belongs_to :users
-  belongs_to :channels
+  belongs_to :user
+  belongs_to :channel
   
-  #validates_uniqueness_of :channel_id, :scope => [ :user_id, :timestamp ]
-  #validates :channel_id, uniqueness: { scope: :user_id, :timestamp } #how to do multiple scope?
+  validates_uniqueness_of :channel_id, :scope => [ :user_id, :timestamp ]
+  #validates :channel_id, uniqueness: { scope: [ :user_id, :timestamp ] } #is this how it would be for Rails 4?
 end
